1cb04dae18e5161ab8b283a158bd68935c2628de
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-09-10 20:56:09 -0400

    Remove debug logging.

--- Sources/Steps/SyntaxHighlighting.swift
+++ Sources/Steps/SyntaxHighlighting.swift
@@ -13,16 +13,11 @@ private extension Modifier {
     static var prism: Self {
         let prism = PrismJS()
     
-        print("prism has these languages: \(prism.languages)")
-    
         return .init(target: .codeBlocks) { html, markdown in
             let openingMarks = markdown.components(separatedBy: .newlines).first ?? "```"
             let language = String(openingMarks.dropFirst("```".count))
             
-            print("Highlighting '\(language)'")
-            
             guard language != "no-language" && !language.isEmpty && prism.languages.contains(language) else {
-                print("lol nvm")
                 return html
             }
             
